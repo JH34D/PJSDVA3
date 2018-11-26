6244fb86b2a3594fbde8e0f523c5e05c3fedae59
JH34D <poschjared@hotmail.com>
2018-11-26 14:20:57 +0100

    php stuffs

---
+++ PHP/filled.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Monitor Drunk Dude</title>
+    <style type="text/css">
+  body,html {
+    padding: 3px 3px 3px 3px;
+
+    background-color: #D8DBE2;
+
+    font-family: Verdana, sans-serif;
+    font-size: 11pt;
+    text-align: center;
+  }
+    </style>
+  </head>
+  <body>
+Hi there. Your input was:
+<?php echo $_POST["txt"]; ?><br>
+The button you chose was:
+<?php echo $_POST["gaga"]; ?><br>
+<a href="index.php">click here</a> to go back to the form page.
+  </body>
+</html>
