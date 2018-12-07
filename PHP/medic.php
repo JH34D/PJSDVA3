2decd858d76030945c1cf51e555a5e0f32cda01b
JH34D <poschjared@hotmail.com>
2018-12-07 13:03:42 +0100

    update of the PHP, files for the medic and porter have been added

---
+++ PHP/medic.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Porter: Monitoring Hank</title>
+    <style type="text/css">
+      body,html {
+        background-color: #D8DBE2;
+
+        font-family: Arial, sans-serif;
+        text-align: center;
+      }
+      div {
+        box-sizing: border-box;
+      }
+      @media only screen and (max-width: 800px) {
+        img {
+          width: 40%;
+          align: left;
+          padding-left: 4%;
+          padding-right: 4%;
+          padding-bottom: 2%;
+        }
+        h1 {font-size: 24pt;}
+      }
+      @media only screen and (min-width: 800px) {
+        img {
+          width: 25%;
+          align: left;
+          padding-left: 3%;
+          padding-right: 3%;
+          padding-bottom: 1%;
+        }
+        h1 {font-size: 36-pt;]
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Monitoring: Hank.</h1></br>
+    <img src="LightOn.png" alt="Lights are on.">
+    <img src="bed.png" alt="Hank is in bed.">
+    <img src="Door.png" alt="Door button">
+    <img src="Fist.png" alt="Hank is currently not aggressive.">
+    <img src="Staff.png" alt="Hank doens't need assistance.">
+    <img src="Emergency.png" alt="There is no alert.">
+  </body>
+</html>
+
+
---
+++ PHP/porter.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Porter: Monitoring Hank</title>
+    <style type="text/css">
+      body,html {
+        background-color: #D8DBE2;
+
+        font-family: Arial, sans-serif;
+      }
+      div {
+        box-sizing: border-box;
+      }
+      @media only screen and (max-width: 800px) {
+        img {
+          width: 40%;
+          align: left;
+          padding-left: 4%;
+          padding-right: 4%;
+          padding-bottom: 2%;
+        }
+        h1 {font-size: 24pt;}
+      }
+      @media only screen and (min-width: 800px) {
+        img {
+          width: 25%;
+          align: left;
+          padding-left: 3%;
+          padding-right: 3%;
+          padding-bottom: 1%;
+        }
+        h1 {font-size: 36-pt;]
+      }
+    </style>
+  </head>
+  <body>
+    <img src="Hank.png" alt="Monitoring: Hank.">
+    <img src="LightOn.png" alt="Lights are on.">
+    <img src="bed.png" alt="Hank is in bed.">
+    <img src="Door.png" alt="Door button">
+    <img src="Staff.png" alt="Hank doens't need assistance.">
+    <img src="Emergency.png" alt="There is no alert.">
+  </body>
+</html>
+
+
