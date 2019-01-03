2aae40eb4b5fc042b41e16cdd4292ea885484ea4
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-03 15:17:44 +0100

    Update Window.cpp

--- socketClient/Source/devices/Window.cpp
+++ socketClient/Source/devices/Window.cpp
@@ -1,1 +1,37 @@
+/*
+ * Window.cpp
+ *
+ *  Created on: Jan 3, 2019
+ *      Author: programmer
+ */
+#include "Window.h"
+#include <iostream>
+#include "../sockClient.h"
+#include "../File.h"
+
+using namespace std;
+using namespace nlohmann;
+
+Window::Window(string ip, int port):Device(ip,port){
+
+}
+
+Window::~Window(){
+	//
+}
+
+void Window::handleActions(){
+	/*
+	 * request inputs
+	 * handle actions and set ouput value in json object
+	 * set outputs
+	 * return json for php?
+	 */
+
+	requestInputs();
+
+
+	setOutputs();
+}
+
 
