c3b8eb8eaba611e75ab07644eee14c73b30d8a04
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-03 15:19:08 +0100

    Update Window.h

--- socketClient/Source/devices/Window.h
+++ socketClient/Source/devices/Window.h
@@ -1,1 +1,25 @@
+/*
+ * Window.h
+ *
+ *  Created on: Jan 3, 2019
+ *      Author: programmer
+ */
+
+#ifndef DEVICES_WINDOW_H_
+#define DEVICES_WINDOW_H_
+
+#include <iostream>
+#include "../Device.h"
+
+class Window : public Device {
+public:
+	Window(string ip, int port);
+	virtual ~Window();
+	void handleActions();
+private:
+
+};
+
+#endif /* DEVICES_WINDOW_H_ */
+
 
