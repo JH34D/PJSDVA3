c3952f463f5d42f4488292484b63591292bd94a5
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-03 15:12:02 +0100

    Update Column.h

--- socketClient/Source/devices/Column.h
+++ socketClient/Source/devices/Column.h
@@ -8,12 +8,18 @@
 #ifndef DEVICES_COLUMN_H_
 #define DEVICES_COLUMN_H_
 
+#include <iostream>
 #include "../Device.h"
 
-class Column {
+class Column : public Device {
 public:
-	Column();
+	Column(string ip, int port);
 	virtual ~Column();
+	void handleActions();
+private:
+	void fireAlarm();
 };
 
 #endif /* DEVICES_COLUMN_H_ */
+
+
