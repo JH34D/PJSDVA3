8f913c79c2f74fa0b03ff3f5bbad3f4c7386d784
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-03 15:11:21 +0100

    Update Column.cpp

--- socketClient/Source/devices/Column.cpp
+++ socketClient/Source/devices/Column.cpp
@@ -4,15 +4,48 @@
  *  Created on: Dec 15, 2018
  *      Author: programmer
  */
-
 #include "Column.h"
+#include <iostream>
+#include "../sockClient.h"
+#include "../File.h"
 
-Column::Column() {
-	// TODO Auto-generated constructor stub
+using namespace std;
+using namespace nlohmann;
+
+Column::Column(string ip, int port):Device(ip,port){
 
 }
 
-Column::~Column() {
-	// TODO Auto-generated destructor stub
+Column::~Column(){
+	//
 }
 
+void Column::handleActions(){
+	/*
+	 * request inputs
+	 * handle actions and set ouput value in json object
+	 * set outputs
+	 * return json for php?
+	 */
+
+	requestInputs();
+	fireAlarm();
+
+	setOutputs();
+}
+
+void Column::fireAlarm(){
+	
+	int fireDetector = inputs.value("fireDetector", 1050); //get value if error return 1050
+		if (fireDetector == 1050){ //check for error
+			cerr << "Error while reading input values. fireDetector could not be found in Json object in function handleBedtime() in class chair" << endl;
+			fireDetector = 0;
+			//return;
+		}
+		if (fireDetector > 200 && fireDetector < 450) {
+			outputs["fireAlarm"] = 0;
+		}
+		else {
+			outputs["fireAlarm"] = 1;
+		}
+}
