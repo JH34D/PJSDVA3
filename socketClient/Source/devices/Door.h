43459253fc4e49479f07b48e3f6daee3f1f5d305
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-07 14:21:04 +0100

    Update Door.h

--- socketClient/Source/devices/Door.h
+++ socketClient/Source/devices/Door.h
@@ -1,1 +1,26 @@
+/*
+ * Door.h
+ *
+ *  Created on: Dec 18, 2018
+ *      Author: programmer
+ */
 
+#ifndef DEVICES_DOOR_H_
+#define DEVICES_DOOR_H_
+#include <iostream>
+#include "../Device.h"
+
+class Door : public Device {
+public:
+	Door(string, int, const char*);
+	virtual ~Door();
+	void handleActions();
+
+private:
+	void handleDoor();
+	void handleFire();
+
+};
+
+
+#endif /* DEVICES_DOOR_H_ */
