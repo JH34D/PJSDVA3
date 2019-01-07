2cae188daeae1f584fb214c6ee5109c03e3bf0da
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-07 14:19:05 +0100

    Update Fridge.h

--- socketClient/Source/devices/Fridge.h
+++ socketClient/Source/devices/Fridge.h
@@ -1,1 +1,26 @@
+/*
+ * Fridge.h
+ *
+ *  Created on: Jan 6, 2019
+ *      Author: programmer
+ */
 
+#ifndef DEVICES_FRIDGE_H_
+#define DEVICES_FRIDGE_H_
+#include <iostream>
+#include "../Device.h"
+
+class Fridge : public Device {
+public:
+	Fridge(string, int, const char*);
+	virtual ~Fridge();
+	void handleActions();
+
+private:
+	void handleFridge();
+	void handleTemp();
+
+};
+
+
+#endif /* DEVICES_FRIDGE_H_ */
