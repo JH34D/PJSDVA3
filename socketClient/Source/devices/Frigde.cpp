b6bb23d8de9a30034d215e44be4dbb0dc0470e7d
Britttt <45041978+Britttt@users.noreply.github.com>
2019-01-07 14:19:59 +0100

    Update Frigde.cpp

--- socketClient/Source/devices/Frigde.cpp
+++ socketClient/Source/devices/Frigde.cpp
@@ -1,1 +1,55 @@
+#include "Fridge.h"
+#include <iostream>
+#include "../sockClient.h"
+#include "../File.h"
 
+using namespace std;
+using namespace nlohmann;
+
+Fridge::Fridge(string ip, int port,const char* path):Device(ip,port,path) {
+	// TODO Auto-generated constructor stub
+
+}
+
+Fridge::~Fridge() {
+	// TODO Auto-generated destructor stub
+}
+
+void Fridge::handleActions(){
+
+	requestInputs();
+	handleFridge();
+	handleTemp();
+	setOutputs();
+}
+void Fridge::handleFridge(){
+	int fridgeSwitch = inputs.value("fridgeSwitch", 20); //get value if error return
+				if (fridgeSwitch == 20){ //check for error
+					cerr << "Error while reading input values. FridgeDoor could not be found in Json object in function handleBedtime() in class Fridge" << endl;
+					fridgeSwitch = 0;
+					//return;
+				}
+				if (fridgeSwitch == 14) {
+					outputs["fridgeDoor"] = 1;
+				}
+				else {
+
+					outputs["fridgeDoor"] = 0;
+				}
+}
+void Fridge::handleTemp(){
+	int tempSensor = inputs.value("tempSensor", 1050);
+	if (tempSensor == 1050){ //check for error
+						cerr << "Error while reading input values. TempSensor could not be found in Json object in function handleBedtime() in class Fridge" << endl;
+						tempSensor = 0;
+						//return;
+					}
+					if (tempSensor < 7) {
+						outputs["fridge"] = 1;
+					}
+					else {
+
+						outputs["fridge"] = 0;
+					}
+
+}
