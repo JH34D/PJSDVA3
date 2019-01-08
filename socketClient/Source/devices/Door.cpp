ba05ba397cc48196cea5947c372782dc750d9060
MikhailSyoufi <MikhailSyoufi@github.com>
2019-01-08 12:40:22 +0100

    Door and Fridge update

--- socketClient/Source/devices/Door.cpp
+++ socketClient/Source/devices/Door.cpp
@@ -32,7 +32,7 @@ void Door::handleActions(){
 
 void Door::handleDoor(){
 	int doorButton = inputs.value("doorButton", 20); //get value if error return 1050
-			if (doorButton == 14){ //check for error
+			if (doorButton == 20){ //check for error
 				cerr << "Error while reading input values. doorButton could not be found in Json object in function handleDoor() in class Door  " << endl;
 				doorButton = 0;
 				//return;
@@ -46,16 +46,24 @@ void Door::handleDoor(){
 			}
 	}
 void Door::handleFire(){
-	int fireDetector = inputs.value("fireDetector", 1050); //get value if error return 1050
-			if (fireDetector == 1050){ //check for error
+	int fireDetector = inputs.value("fireDetector", 20); //get value if error return 1050
+			if (fireDetector == 20){ //check for error
 				cerr << "Error while reading input values. fireDetector could not be found in Json object in function handleFire in class Door" << endl;
 				fireDetector = 0;
 			}
 			if (fireDetector > 450) { //above 450 there is fire detected
-				outputs["openDoor"] = 1; //if fire then the door opens
+				outputs["openDoor"] = 10; //if fire then the door opens
 			}
 			else {
-				outputs["openDoor"] = 0;
+				outputs["openDoor"] = 80;
 			}
 }
 
+void Door::handlePhp(){
+	int phpNumber;
+			if (phpNumber == 11){
+				outputs["phpOpen"] = 10;
+			}else if (phpNumber == 22){
+				outputs["phpOpen"] = 80;
+			}
+}
